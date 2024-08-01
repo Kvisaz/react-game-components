01129580d10ddbf53a11eb3cecaa3f1bf5ab5570
sergeytokarev <stokarev@novakidschool.com>
2024-08-01 19:35:05 +0700

    storybook init

---
+++ src/components/ButtonTest/index.tsx
@@ -0,0 +1,12 @@
+interface ButtonProps {
+    text: string;
+    onClick: () => void;
+}
+
+export const ButtonTest = ({ text, onClick }: ButtonProps) => {
+    return (
+        <button onClick={onClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            {text}
+        </button>
+    );
+};

2e13f4310fa738eae92bec8e4368201f70367f3a
Sergey Tokarev <sergey.zoomaster@gmail.com>
2024-08-06 14:54:05 +0700

    Deploy to GitHub Pages

---
+++ docs/storybook/assets/ArticleTest.stories-CTCYZwfi.js
@@ -0,0 +1,11 @@
+import{j as e,T as t}from"./index-BCMmJj2x.js";import"./index-DJO9vBfz.js";const m=({hText:p,list:l})=>e.jsxs("article",{children:[e.jsx("h1",{children:e.jsx(t,{text:p})}),e.jsx("ol",{children:l.map((T,x)=>e.jsx("li",{children:e.jsx(t,{text:T})},"ArticleTest"+x))})]});m.__docgenInfo={description:"",methods:[],displayName:"ArticleTest",props:{hText:{required:!0,tsType:{name:"string"},description:""},list:{required:!0,tsType:{name:"Array",elements:[{name:"string"}],raw:"Array<string>"},description:""}}};const y={title:"Components/ArticleTest",component:m},r={args:{hText:"Primary!",list:["1. Test!","2. Test2!","2. Primary!","4. Added through story."]}},s={args:{hText:"Second!",list:["1. Second!","2. Second!","2. Second!"]}};var o,n,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    hText: "Primary!",
+    list: ["1. Test!", "2. Test2!", "2. Primary!", "4. Added through story."]
+  }
+}`,...(a=(n=r.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};var i,c,d;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    hText: "Second!",
+    list: ["1. Second!", "2. Second!", "2. Second!"]
+  }
+}`,...(d=(c=s.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};const g=["Primary","Second"];export{r as Primary,s as Second,g as __namedExportsOrder,y as default};

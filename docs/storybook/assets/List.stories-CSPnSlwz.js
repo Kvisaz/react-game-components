6e899f3f1ba790348fb4e472ee68b0994ca90f84
Dedede <keeperofwonderland@gmail.com>
2024-08-28 21:58:45 +0700

    Deploy to GitHub Pages

---
+++ docs/storybook/assets/List.stories-CSPnSlwz.js
@@ -0,0 +1,21 @@
+import{j as n}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const p=({items:l=[{name:"apple",key:"lis-apple"},{name:"banana",key:"list-banana"},{name:"orange",key:"list-orange"}]})=>n.jsx("ul",{children:l.map(({name:c,key:y})=>n.jsx("li",{children:c},y))});p.__docgenInfo={description:"",methods:[],displayName:"List",props:{items:{required:!1,tsType:{name:"Array",elements:[{name:"signature",type:"object",raw:"{ name: string; key: string }",signature:{properties:[{key:"name",value:{name:"string",required:!0}},{key:"key",value:{name:"string",required:!0}}]}}],raw:"Array<{ name: string; key: string }>"},description:"",defaultValue:{value:`[\r
+  { name: "apple", key: "lis-apple" },\r
+  { name: "banana", key: "list-banana" },\r
+  { name: "orange", key: "list-orange" },\r
+]`,computed:!1}}}};const k={title:"Components/001-28-08-2024-react-katas/List",component:p},e={},a={args:{items:[{name:"молоко",key:"list-молоко"},{name:"хлеб",key:"list-хлеб"},{name:"огурцы",key:"list-огурцы"},{name:"халва",key:"list-халва"}]}};var r,s,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var i,o,m;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      name: "молоко",
+      key: "list-молоко"
+    }, {
+      name: "хлеб",
+      key: "list-хлеб"
+    }, {
+      name: "огурцы",
+      key: "list-огурцы"
+    }, {
+      name: "халва",
+      key: "list-халва"
+    }]
+  }
+}`,...(m=(o=a.parameters)==null?void 0:o.docs)==null?void 0:m.source}}};const g=["DefaultStory","ShoppingList"];export{e as DefaultStory,a as ShoppingList,g as __namedExportsOrder,k as default};

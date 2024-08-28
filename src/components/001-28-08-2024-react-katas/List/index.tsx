05162117c234bc09c6ef1829f188a9de8fd1ceb2
Dedede <keeperofwonderland@gmail.com>
2024-08-28 21:53:28 +0700

    main - fixed keys in list
    main - fixed keys in list

--- src/components/001-28-08-2024-react-katas/List/index.tsx
+++ src/components/001-28-08-2024-react-katas/List/index.tsx
@@ -5,8 +5,8 @@ interface ListProps {
 export const List = ({ items = ["apple", "banana", "orange"] }: ListProps) => {
   return (
     <ul>
-      {items.map((text) => (
-        <li key={new Date().getTime()}>{text}</li>
+      {items.map((text, index) => (
+        <li key={index}>{text}</li>
       ))}
     </ul>
   );

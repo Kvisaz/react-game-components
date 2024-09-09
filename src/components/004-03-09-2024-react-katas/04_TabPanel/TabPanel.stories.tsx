f0a8c29d251508dcc0656f6019e18ea607115b39
Dedede <keeperofwonderland@gmail.com>
2024-09-09 20:39:23 +0700

    main - Components 004-03-09-2024-react-katas TabPanel added args to story
    main - Components 004-03-09-2024-react-katas TabPanel added args to story

--- src/components/004-03-09-2024-react-katas/04_TabPanel/TabPanel.stories.tsx
+++ src/components/004-03-09-2024-react-katas/04_TabPanel/TabPanel.stories.tsx
@@ -4,10 +4,31 @@ import { TabPanel } from ".";
 const meta: Meta<typeof TabPanel> = {
   title: "Components/004-03-09-2024-react-katas/TabPanel",
   component: TabPanel,
+  argTypes: { tabs: { control: "object" } },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof TabPanel>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    tabs: [
+      {
+        name: "Главная",
+        content: "Содержимое главной вкладки",
+        key: "test-tab-01-key",
+      },
+      {
+        name: "О нас",
+        content: "Информация о компании",
+        key: "test-tab-02-key",
+      },
+      {
+        name: "Контакты",
+        content: "Наши контактные данные",
+        key: "test-tab-03-key",
+      },
+    ],
+  },
+};

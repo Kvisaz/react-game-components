476e4340acd15533b3e710ce14e85157adb9c334
Dedede <keeperofwonderland@gmail.com>
2024-09-13 19:18:32 +0700

    Deploy to GitHub Pages

---
+++ docs/storybook/assets/OnlineStatus.stories-DiRoXI9N.js
@@ -0,0 +1,5 @@
+import{j as i}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const c="_box_1iro2_1",l={box:c},o=({isOnline:r})=>{const a={"--color":r?"green":"red"};return i.jsx("div",{className:l.box,style:a})};o.__docgenInfo={description:"",methods:[],displayName:"OnlineStatus",props:{isOnline:{required:!0,tsType:{name:"boolean"},description:""}}};const u={title:"Components/005-10-09-2024-react-katas/OnlineStatus",component:o},e={args:{isOnline:!0}};var t,s,n;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    isOnline: true
+  }
+}`,...(n=(s=e.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,u as default};
